33c6b546700ac1d0b2238bf333340f0c21358ab2
Joaquim Barrera <quim.barrera@pm.me>
2021-11-30 00:12:52 +0100

    Version bump 1.3

--- src/Thermostat.cpp
+++ src/Thermostat.cpp
@@ -13,7 +13,7 @@
 
 /*-------------------------------- Defines -----------------------------------*/
 #define APPNAME_STR "Thermostat"
-#define RELEASE_STR "1.2"
+#define RELEASE_STR "1.3"
 
 #define WITH_RFM69
 #define WITH_SPIFLASH
